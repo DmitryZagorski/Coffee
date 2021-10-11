54349033ed12e613f2cc820508aaab87892b58b3
DmitryZagorski <d375298856600@ya.ru>
2021-10-11 19:24:35 +0300

    Change level of loggers

--- src/main/java/com/epam/coffeewagon/main/Main.java
+++ src/main/java/com/epam/coffeewagon/main/Main.java
@@ -10,7 +10,6 @@ import com.epam.coffeewagon.wagon.WagonService;
 import com.epam.coffeewagon.wagon.WagonServiceInterface;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 
 public class Main {
